c67069702bfb3c20c3a6428474f53c892f243e36
lreynolds188 <lreynolds188@gmail.com>
2016-05-08 16:17:16 +1000

    Create Message.java

---
+++ Server/Message.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Server;
+
+/**
+ *
+ * @author student
+ */
+public class Message {
+    String msg;
+    
+    public Message(String _msg){
+        msg = _msg;
+    }
+}
